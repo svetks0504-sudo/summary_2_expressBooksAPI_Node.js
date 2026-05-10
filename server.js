71533ed809b86773342715cdf4d95fed508cacf7
Svitlana <svetks0504@gmail.com>
2026-05-10 22:04:06 +0200

    add get, post and put

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get("/books", (req, res) => {
   const { genre, author, available, limit, offset } = req.query;
 
   if (genre) {
-    result = filterByField(result,"genre", genre);
+    result = filterByField(result, "genre", genre);
   }
   if (author) {
     result = filterByField(result, "author", author);
@@ -35,21 +35,88 @@ app.get("/books", (req, res) => {
   if (available) {
     const isAvailable = available === "true";
 
-   result = result.filter((book) => {
-       return book.isAvailable === isAvailable;
-    })
+    result = result.filter((book) => {
+      return book.isAvailable === isAvailable;
+    });
   }
 
   const start = Number(offset) || 0;
   const end = limit ? start + Number(limit) : undefined;
 
-  res.json(result.slice(start, end));//передаем клиенту
+  res.json(result.slice(start, end)); //передаем клиенту
 });
+
+app.get("/books/stats/genres", (req, res) => {
+  const stats = books.reduce((acc, book) => {
+    acc[book.genre] = acc[book.genre] ? acc[book.genre] + 1 : 1;
+    return acc;
+  }, {});
+  res.json(stats);
+});
+
+app.get("/books/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const book = books.find((book) => book.id === id);
+
+  if (!book) {
+    return res.status(404).json({ message: "Book not found" });
+  }
+  res.json(book);
+});
+
+app.post("/books", (req, res) => {
+  const { title, author, year, genre, isAvailable } = req.body;
+
+  if (
+    !title ||
+    !author ||
+    year === undefined ||
+    !genre ||
+    isAvailable === undefined
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  if (year < 1500 || year > 2025) {
+    return res
+      .status(400)
+      .json({ message: "Year must be between 1500 and 2025" });
+  }
+  const newBook = {
+    id: books.length ? books[books.length - 1].id + 1 : 1,
+    title,
+    author,
+    year,
+    genre,
+    isAvailable,
+  };
+  books.push(newBook);
+  res.status(201).json(newBook);
+});
+
+app.put("/books/:id", (req, res) => {
+  const { title, author, year, genre, isAvailable } = req.body;
+  const id = Number(req.params.id);
+
+  const book = find.books((book) => book.id === id);
+  if (!id) {
+    return res.status(404).json({ message: "id not found" });
+  }
+  book.title = title;
+  book.author = author;
+  book.year = year;
+  book.genre = genre;
+  book.isAvailable = isAvailable;
+
+  res.json(book);
+});
+
+
 
 app.listen(port, host, () => {
   console.log(`Server running at http://${host}:${port}`);
 });
 
 function filterByField(arr, key, value) {
- return arr.filter((item) => item[key] === value);
+  return arr.filter((item) => item[key] === value);
 }
